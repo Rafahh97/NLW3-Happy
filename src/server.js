0bf6c1a105494356c6fe2b97f1a03c7280cd0270
Rafaela Gomes <rafahh97@gmail.com>
2020-10-15 14:43:47 -0300

    create paths

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 // Importar dependência
 const express = require('express');
 const path = require('path');
+const pages = require('./pages.js');
 
 // Iniciando o express
 const server = express()
@@ -8,10 +9,15 @@ server
 // Utilizando os arquivos estáticos
 .use(express.static('public'))
 
-// Criar uma rota
-server.get('/', (request, response) => {
-    return response.sendFile(path.join(__dirname, 'views', 'index.html'));
-})
+// Configurar template engine
+.set('views', path.join(__dirname, 'views'))
+.set('view engine', 'hbs')
+
+// Rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage) 
 
 // Ligar o servidor
 server.listen(5500)
