3c08ff8bb2525dd82c042bfbe00bddf4696edf0b
Rafaela Gomes <rafahh97@gmail.com>
2020-10-15 13:45:03 -0300

    use static files

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 // Importar dependência
 const express = require('express');
+const path = require('path');
+
+// Iniciando o express
 const server = express()
+server
+// Utilizando os arquivos estáticos
+.use(express.static('public'))
 
 // Criar uma rota
-server.get('/', () => {
-    return console.log('oi')
+server.get('/', (request, response) => {
+    return response.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
 // Ligar o servidor
