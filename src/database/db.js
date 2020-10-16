7ce329e928e698714092aa1d0a18b6b3a4b33abd
Rafaela Gomes <rafahh97@gmail.com>
2020-10-16 19:52:45 -0300

    fix and improve

--- src/database/db.js
+++ src/database/db.js
@@ -1,20 +1,20 @@
-const Database = require('sqlite-async');
-
-Database.open(__dirname + '/database.sqlite').then(execute)
+const Database = require("sqlite-async");
 
 function execute(db) {
-   db.exec(`
-      CREATE TABLE IF NOT EXISTS orphanages (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          Lat TEXT,
-          Lng TEXT,
-          name TEXT,
-          about TEXT,
-          whatsapp TEXT,
-          images TEXT,
-          instructions TEXT,
-          opening_hours TEXT,
-          open_on_weekends TEXT
-      );
-    `) 
+  return db.exec(`
+        CREATE TABLE IF NOT EXISTS orphanages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            lat TEXT,
+            lng TEXT,
+            name TEXT,
+            about TEXT,
+            whatsapp TEXT,
+            images TEXT,
+            instructions TEXT,
+            opening_hours TEXT,
+            open_on_weekends TEXT
+        );
+    `);
 }
+
+module.exports = Database.open(__dirname + "/database.sqlite").then(execute);
