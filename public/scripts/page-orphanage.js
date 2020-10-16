ff56e8a5de4c394032d334fba0bdb34478073e4b
Rafaela Gomes <rafahh97@gmail.com>
2020-10-16 19:49:30 -0300

    fix map

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -5,8 +5,13 @@ const options = {
     scrollWheelZoom: false,
     zoomControl: false,
 }
+// get values from html
+const lat = document.querySelector("span[data-lat]").dataset.lat;
+const lng = document.querySelector("span[data-lng]").dataset.lng;
+
 //create map
-const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+const map = L.map('mapid', options).setView([lat, lng], 15);
+
 //create and add tileLayer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 //create icon  
@@ -17,7 +22,8 @@ const icon = L.icon({
   popAnchor: [170, 2]
 })
 //create and add marker
-L.marker([-27.222633, -49.6455874], { icon }).addTo(map)
+
+L.marker([lat, lng], { icon }).addTo(map)
 
 // image gallery
 function selectImage(event) {
